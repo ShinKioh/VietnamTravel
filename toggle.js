6e68885ccab70c67c33c56927fba7913fe55f2d1
SHIN KI OH <te4328@gmai.com>
2024-06-22 23:34:35 +0900

    update

---
+++ toggle.js
@@ -0,0 +1,47 @@
+function dalatToggle() {
+  if (document.getElementById('dalatToggle').style.display == 'block') {
+    document.getElementById('dalatToggle').style.display = 'none';
+  } else {
+    document.getElementById('dalatToggle').style.display = 'block';
+  }
+}
+
+function nhaTrangToggle() {
+  if (document.getElementById('nhaTrangToggle').style.display == 'block') {
+    document.getElementById('nhaTrangToggle').style.display = 'none';
+  } else {
+    document.getElementById('nhaTrangToggle').style.display = 'block';
+  }
+}
+
+function vinPearlToggle() {
+  if (document.getElementById('vinPearlToggle').style.display == 'block') {
+    document.getElementById('vinPearlToggle').style.display = 'none';
+  } else {
+    document.getElementById('vinPearlToggle').style.display = 'block';
+  }
+}
+
+function lastDayToggle() {
+  if (document.getElementById('lastDayToggle').style.display == 'block') {
+    document.getElementById('lastDayToggle').style.display = 'none';
+  } else {
+    document.getElementById('lastDayToggle').style.display = 'block';
+  }
+}
+
+function dalatReastaurantToggle() {
+  if (document.getElementById('dalatReastaurantToggle').style.display == 'block') {
+    document.getElementById('dalatReastaurantToggle').style.display = 'none';
+  } else {
+    document.getElementById('dalatReastaurantToggle').style.display = 'block';
+  }
+}
+
+function natTrangReastaurantToggle() {
+  if (document.getElementById('natTrangReastaurantToggle').style.display == 'block') {
+    document.getElementById('natTrangReastaurantToggle').style.display = 'none';
+  } else {
+    document.getElementById('natTrangReastaurantToggle').style.display = 'block';
+  }
+}

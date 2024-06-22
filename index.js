d4f3a07563eda92669af4e0c19cc43e10f558f89
SHIN KI OH <te4328@gmai.com>
2024-06-22 22:08:09 +0900

    js update

---
+++ index.js
@@ -0,0 +1,59 @@
+function Aramandara() {
+  window.open(
+    'https://www.agoda.com/ko-kr/ana-mandara-villas-dalat-resort-spa/hotel/dalat-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1755781&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-21&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=13ee1f40-1de4-434b-a998-0c5fcefe2ebf&los=2&searchrequestid=de54449e-e06a-42ba-8779-3df77bbb4adb&ds=WlKzaO6Lto0axeZ5'
+  );
+}
+
+function PanoramaStarBeachNhaTrang() {
+  window.open(
+    'https://www.agoda.com/ko-kr/vinpearl-resort-spa-nha-trang-bay/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1755781&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-25&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=13ee1f40-1de4-434b-a998-0c5fcefe2ebf&los=1&searchrequestid=82c753c0-9f04-4af2-b9be-73abe582deca&ds=loxlGbj5L3QyUAcl'
+  );
+}
+
+function RegaliaGoldHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/regalia-gold-hotel/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1770664&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-23&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tspTypes=1%2C8&los=1&searchrequestid=e1e9da31-ee4e-4df8-93e4-396354a9d6e2&ds=BFATLOpvvNyEPglF'
+  );
+}
+
+function VirgoHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/virgo-hotel/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1770664&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-23&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tspTypes=2%2C8&los=1&searchrequestid=e1e9da31-ee4e-4df8-93e4-396354a9d6e2&ds=BFATLOpvvNyEPglF'
+  );
+}
+
+function HavanaNhaTrangHotel() {
+  window.open(
+    'https://www.agoda.com/ko-kr/havana-nha-trang-hotel/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1770664&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-23&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tspTypes=8&los=1&searchrequestid=e1e9da31-ee4e-4df8-93e4-396354a9d6e2&ds=msGADexPEEZdtxPH'
+  );
+}
+
+function WiseStayGoldCoast() {
+  window.open(
+    'https://www.agoda.com/ko-kr/wise-stay-gold-coast/hotel/nha-trang-vn.html?los=1&pid=redirect&adults=2&deep_link_value=agoda%3A%2F%2Fhotel%2FWise%20Stay%20Gold%20Coast%2F31664313&cid=1770664&c=Property_Sharing_V1_Shortened&rooms=1&site_id=1770664&ds=BFATLOpvvNyEPglF&checkin=2024-07-27'
+  );
+}
+
+function HoppingTourAngel() {
+  window.open('https://cafe.naver.com/frpolice/16979');
+}
+
+function HoppingTourSantiago() {
+  window.open('https://smartstore.naver.com/nhatrangtour/products/6902281039?');
+}
+
+function VinpearlResortSpaNhaTrangBay() {
+  window.open(
+    'https://www.agoda.com/ko-kr/vinpearl-resort-spa-nha-trang-bay/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1755781&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-25&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=13ee1f40-1de4-434b-a998-0c5fcefe2ebf&los=1&searchrequestid=82c753c0-9f04-4af2-b9be-73abe582deca&ds=loxlGbj5L3QyUAcl'
+  );
+}
+
+function VinpearlResortNhaTrang() {
+  window.open(
+    'https://www.agoda.com/ko-kr/vinpearl-resort-nha-trang/hotel/nha-trang-vn.html?finalPriceView=1&isShowMobileAppPrice=false&cid=1770664&numberOfBedrooms=&familyMode=false&adults=2&children=0&rooms=1&maxRooms=0&checkIn=2024-07-25&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=1&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tspTypes=7%2C6%2C8&los=1&searchrequestid=9d2316ae-670a-46bd-913d-6b9918698a07&ds=BFATLOpvvNyEPglF'
+  );
+}
+
+function StarInPanrang() {
+  window.open('https://www.myrealtrip.com/offers/131864');
+}
